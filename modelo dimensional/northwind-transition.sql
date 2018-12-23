ebb8eddb896ccbca5fdd1edf238d9ee1807537ad
Carlos Campos <31832440+czgcampos@users.noreply.github.com>
2018-12-23 15:09:41 +0000

    Add files via upload

--- modelo dimensional/northwind-transition.sql
+++ modelo dimensional/northwind-transition.sql
@@ -1,24 +1,44 @@
-INSERT INTO northwind_dw.dim_cliente (idCliente,nome,empresa,cidade,estado)
+INSERT INTO northwind_dw.dim_cliente
 	SELECT id, CONCAT(first_name," ",last_name), company, city, state_province FROM northwind.customers;
 
-INSERT INTO northwind_dw.dim_expedidor (idExpedidor,empresa)
+INSERT INTO northwind_dw.dim_expedidor
 	SELECT id, company FROM northwind.shippers;
     
-INSERT INTO northwind_dw.dim_funcionario (idFuncionario,nome,cargo,cidade)
+INSERT INTO northwind_dw.dim_funcionario
 	SELECT id, CONCAT(first_name," ",last_name), job_title, city FROM northwind.employees;
 
-INSERT INTO northwind_dw.dim_fornecedor (idFornecedor,empresa)
+INSERT INTO northwind_dw.dim_fornecedor
 	SELECT id, company FROM northwind.suppliers;
     
-INSERT INTO northwind_dw.dim_produto (idProduto,nome,quantidadeUnidade,categoria)
+INSERT INTO northwind_dw.dim_produto
 	SELECT id, product_name, quantity_per_unit, category FROM northwind.products;
     
-INSERT INTO northwind_dw.dim_data (data,diaSemana,dia,mes,ano,trimestre,semestre)
-	SELECT order_date, WEEKDAY(order_date), DAY(order_date), MONTH(order_date), YEAR(order_date), QUARTER(order_date), FLOOR((QUARTER(order_date)/3)+1) FROM northwind.orders WHERE order_date IS NOT NULL
+INSERT INTO northwind_dw.dim_data
+	SELECT CAST(CONCAT(YEAR(order_date),MONTH(order_date),DAY(order_date),HOUR(order_date),MINUTE(order_date),SECOND(order_date)) AS UNSIGNED),
+    order_date, WEEKDAY(order_date), DAY(order_date), MONTH(order_date), YEAR(order_date), QUARTER(order_date), FLOOR((QUARTER(order_date)/3)+1)
+    FROM northwind.orders WHERE order_date IS NOT NULL
 	UNION 
-	SELECT shipped_date, WEEKDAY(shipped_date), DAY(shipped_date), MONTH(shipped_date), YEAR(shipped_date), QUARTER(shipped_date), FLOOR((QUARTER(shipped_date)/3)+1) FROM northwind.orders WHERE shipped_date IS NOT NULL
+	SELECT CAST(CONCAT(YEAR(shipped_date),MONTH(shipped_date),DAY(shipped_date),HOUR(shipped_date),MINUTE(shipped_date),SECOND(shipped_date)) AS UNSIGNED),
+    shipped_date, WEEKDAY(shipped_date), DAY(shipped_date), MONTH(shipped_date), YEAR(shipped_date), QUARTER(shipped_date), FLOOR((QUARTER(shipped_date)/3)+1)
+    FROM northwind.orders WHERE shipped_date IS NOT NULL
 	UNION 
-	SELECT paid_date, WEEKDAY(paid_date), DAY(paid_date), MONTH(paid_date), YEAR(paid_date), QUARTER(paid_date), FLOOR((QUARTER(paid_date)/3)+1) FROM northwind.orders WHERE paid_date IS NOT NULL;
+	SELECT CAST(CONCAT(YEAR(paid_date),MONTH(paid_date),DAY(paid_date),HOUR(paid_date),MINUTE(paid_date),SECOND(paid_date)) AS UNSIGNED),
+    paid_date, WEEKDAY(paid_date), DAY(paid_date), MONTH(paid_date), YEAR(paid_date), QUARTER(paid_date), FLOOR((QUARTER(paid_date)/3)+1)
+    FROM northwind.orders WHERE paid_date IS NOT NULL;
 
-SELECT quantity, (unit_price*quantity)-(unit_price*quantity*discount), unit_price, shipping_fee, discount FROM northwind.orders, northwind.order_details
-	WHERE northwind.order_details.order_id=northwind.orders.id;
+INSERT INTO northwind_dw.tf_encomendas (produto,fornecedor,expedidor,funcionario,cliente,dataPedido,dataPagamento,dataEnvio,quantidade,precoTotal,precoUnidade,precoEnvio,desconto)
+	SELECT od.product_id, CAST(SUBSTRING_INDEX(p.supplier_ids,';',1) AS UNSIGNED),o.shipper_id, o.employee_id, o.customer_id, 
+	CAST(CONCAT(YEAR(o.order_date),MONTH(o.order_date),DAY(o.order_date),HOUR(o.order_date),MINUTE(o.order_date),SECOND(o.order_date)) AS UNSIGNED),
+	CAST(CONCAT(YEAR(o.paid_date),MONTH(o.paid_date),DAY(o.paid_date),HOUR(o.paid_date),MINUTE(o.paid_date),SECOND(o.paid_date)) AS UNSIGNED),
+	CAST(CONCAT(YEAR(o.shipped_date),MONTH(o.shipped_date),DAY(o.shipped_date),HOUR(o.shipped_date),MINUTE(o.shipped_date),SECOND(o.shipped_date)) AS UNSIGNED),
+	od.quantity, (od.unit_price*od.quantity)-(od.unit_price*od.quantity*od.discount), od.unit_price, o.shipping_fee, od.discount 
+	FROM northwind.orders AS o, northwind.order_details AS od, northwind.products AS p
+		WHERE od.order_id=o.id AND od.product_id=p.id
+	UNION
+	SELECT od.product_id, CAST(SUBSTRING_INDEX(p.supplier_ids,';',-1) AS UNSIGNED),o.shipper_id, o.employee_id, o.customer_id, 
+	CAST(CONCAT(YEAR(o.order_date),MONTH(o.order_date),DAY(o.order_date),HOUR(o.order_date),MINUTE(o.order_date),SECOND(o.order_date)) AS UNSIGNED),
+	CAST(CONCAT(YEAR(o.paid_date),MONTH(o.paid_date),DAY(o.paid_date),HOUR(o.paid_date),MINUTE(o.paid_date),SECOND(o.paid_date)) AS UNSIGNED),
+	CAST(CONCAT(YEAR(o.shipped_date),MONTH(o.shipped_date),DAY(o.shipped_date),HOUR(o.shipped_date),MINUTE(o.shipped_date),SECOND(o.shipped_date)) AS UNSIGNED),
+	od.quantity, (od.unit_price*od.quantity)-(od.unit_price*od.quantity*od.discount), od.unit_price, o.shipping_fee, od.discount 
+	FROM northwind.orders AS o, northwind.order_details AS od, northwind.products AS p
+		WHERE od.order_id=o.id AND od.product_id=p.id;
